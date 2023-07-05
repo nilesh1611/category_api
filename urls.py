2904f88103a3b93bd70d4ad37567ab0f206d6752
Nilesh manik shinde <90515512+nilesh1611@users.noreply.github.com>
2023-07-05 18:45:27 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+# from . import views
+from rest_framework.decorators import api_view
+from Item.product import views
+
+urlpatterns = [
+    path('', views.ApiOverview, name='home'),
+    path('create/', views.add_items, name='add-items'),
+    path('all/', views.view_items, name='view_items'),
+    path('update/<int:pk>/', views.update_items, name='update-items'),
+    path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
+
+]
